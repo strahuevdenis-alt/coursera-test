626d73eb94b9e79d98cffa54369222376b824f81
strahuevdenis-alt <strahuevdenis@gmail.com>
2026-04-06 17:58:41 +0500

    Create script.js

---
+++ module4-solution/script.js
@@ -0,0 +1,16 @@
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i = 0; i < names.length; i++) {
+
+    // Извлекаем первую букву и переводим в нижний регистр для надежности
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    // Сравниваем букву с 'j'
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
